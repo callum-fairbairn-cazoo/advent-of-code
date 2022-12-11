8d882f87ba369ab721e805b75c3fb7d1435ab323
Callum Fairbairn <86301717+callum-fairbairn-cazoo@users.noreply.github.com>
2022-12-11 16:14:07 +0000

    further improve template

--- typescript-template/src/app.ts
+++ typescript-template/src/app.ts
@@ -4,10 +4,12 @@ const mapFn = (line) => line
 
 const filterFn = (line) => line !== ""
 
-let data;
+let data: { test: any[], input: any[] };
 try {
-  const raw_data = fs.readFileSync('input.txt', 'utf8');
-  data = raw_data.split("\n").filter(filterFn).map(mapFn)
+  const raw_test_data = fs.readFileSync('test.txt', 'utf8');
+  const raw_input_data = fs.readFileSync('input.txt', 'utf8');
+  data.test = raw_test_data.split("\n").filter(filterFn).map(mapFn)
+  data.input = raw_input_data.split("\n").filter(filterFn).map(mapFn)
 } catch (e) {
   console.log('Error:', e.stack);
 }
