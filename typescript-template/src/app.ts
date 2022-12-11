3bd95d63d326cc2d090ef58bf1d6b9ca6a4abed2
Callum Fairbairn <86301717+callum-fairbairn-cazoo@users.noreply.github.com>
2022-12-11 15:58:03 +0000

    improve template

---
+++ typescript-template/src/app.ts
@@ -0,0 +1,13 @@
+import * as fs from "fs"
+
+const mapFn = (line) => line.split(" ")
+
+const filterFn = (line) => line !== ""
+
+let data;
+try {
+  const raw_data = fs.readFileSync('input.txt', 'utf8');
+  data = raw_data.split("\n").filter(filterFn).map(mapFn)
+} catch (e) {
+  console.log('Error:', e.stack);
+}
